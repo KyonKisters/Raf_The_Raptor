21b6ba64da0ad228a0a65eb5d0bc2684eb6cce19
KyonKisters <kyonkisters@gmail.com>
2021-02-11 11:57:17 +0100

    EAAProjectile destroys when collision
    EAAProjectile destroys when it detect a collision with Tiled map or enemy

--- GXPEngine/GXPEngine/EAAProjectile.cs
+++ GXPEngine/GXPEngine/EAAProjectile.cs
@@ -2,21 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using GXPEngine.Core;
 namespace GXPEngine
 {
     public class EAAProjectile : Sprite
     {
         int lifetime;
+        Player player;
+        Enemy enemy;
         public EAAProjectile() : base("EAAhitbox.png") 
         {
             SetOrigin(width / 2, height / 2);
         }
         void Update()
         {
-            x+=20;
-            lifetime++;
-            if (lifetime > 2)
+            float moveX = 0;
+            float moveY = 0;
+            //x += 20;
+            if (game.FindObjectOfType(typeof(Enemy)).x < game.FindObjectOfType(typeof(Player)).x)
+            {
+                moveX -=5;
+            }
+            if (game.FindObjectOfType(typeof(Enemy)).x > game.FindObjectOfType(typeof(Player)).x)
+            {
+                moveX +=5;
+            }
+            if (game.FindObjectOfType(typeof(Enemy)).y < game.FindObjectOfType(typeof(Player)).y)
+            {
+                moveY -=5;
+            }
+            if (game.FindObjectOfType(typeof(Enemy)).y > game.FindObjectOfType(typeof(Player)).y)
+            {
+                moveY +=5;
+            }
+            Collision collision = MoveUntilCollision(moveX, moveY);
+            if (collision != null)
             {
                 LateDestroy();
             }
